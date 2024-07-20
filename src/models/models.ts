dc7433ecd6075e1895613dd0f85e6be81f13c35a
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-07-20 14:06:04 +0530

    feat: create models.ts and taskSchema

---
+++ src/models/models.ts
@@ -0,0 +1,29 @@
+import { model, Schema } from "mongoose";
+
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["TODO", "IN_PROGRESS", "DONE"],
+      default: "TODO",
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Task = model("Task", taskSchema);
+
+export { Task };
