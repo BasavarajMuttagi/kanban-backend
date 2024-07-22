18865081c106914de7ddc5a08444bd5c4a86c3b9
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-07-23 00:43:14 +0530

    create login, signup schemas

---
+++ src/zod/schema.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+
+const userLoginSchema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "password cannot be less than 8 digits" })
+    .max(10, { message: "password cannot be more than 10 digits" }),
+});
+
+type userLoginType = z.infer<typeof userLoginSchema>;
+
+const userSignUpSchema = z
+  .object({
+    firstname: z.string().min(1, { message: "first name is required" }),
+    lastname: z.string().min(1, { message: "last name is required" }),
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(8, { message: "password cannot be less than 8 digits" })
+      .max(10, { message: "password cannot be more than 10 digits" }),
+    confirmpassword: z
+      .string()
+      .min(8, { message: "password cannot be less than 8 digits" })
+      .max(10, { message: "password cannot be more than 10 digits" }),
+  })
+  .refine((data) => data.password === data.confirmpassword, {
+    message: "Passwords don't match",
+    path: ["confirmpassword"],
+  });
+
+type userSignUpType = z.infer<typeof userSignUpSchema>;
+export type { userLoginType, userSignUpType };
+export { userLoginSchema, userSignUpSchema };
